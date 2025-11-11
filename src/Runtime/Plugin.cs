e26d4f4403c71e67e8080549ced74969cd7a02bd
Jack Burkhardt <git@jackburkhardt.com>
2025-11-10 18:15:06 -0600

    bump: 0.1.1-indev

--- src/Runtime/Plugin.cs
+++ src/Runtime/Plugin.cs
@@ -7,7 +7,7 @@ namespace DiscoAPI.Runtime;
 [BepInPlugin(
     GUID,
     "Disco Elysium Modding API",
-    "0.1.0-indev"
+    "0.1.1-indev"
 )]
 [BepInProcess("disco.exe")]
 public class DiscoAPIPlugin : BasePlugin
